22980d28df65ce72df80f6c3afe0b383a4d5acfb
Ian Holloway <ian.m.holloway@gmail.com>
2023-12-03 00:04:42 -0800

    Added TODO task to discord

--- modules/programs/discord.nix
+++ modules/programs/discord.nix
@@ -3,6 +3,7 @@
   unstable,
   ...
 }: {
+  # TODO: add option to enable/disable discord
   environment.systemPackages = with unstable; [
     # override the discord package to add extensions
     (discord.override {
