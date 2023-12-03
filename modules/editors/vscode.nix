a9c9f115f6add7f23ba731953739dda31f5b566d
Ian Holloway <ian.m.holloway@gmail.com>
2023-12-02 23:54:37 -0800

    Major Improvements to vscode now vscodium

--- modules/editors/vscode.nix
+++ modules/editors/vscode.nix
@@ -3,6 +3,7 @@
   unstable,
   vars,
   inputs,
+  config,
   ...
 }: {
   home-manager.users.${vars.user} = {
@@ -12,27 +13,62 @@
       mutableExtensionsDir = false;
       enableExtensionUpdateCheck = false;
       userSettings = {
-        "git.openRepositoryInParentFolders" = "always";
-        "files.autoSave" = "onFocusChange";
-        "editor.tabSize" = 4;
-        "[nix]"."editor.tabSize" = 2;
+        # Miscellanious
+        "window.titleBarStyle" = "custom"; # Helps with stability on wayland
+        "files.autoSave" = "onFocusChange"; # Save on change of focus i.e. switching files/windows
+        "editor.formatOnSave" = true; # Format on save (may be annoying for some people)
+        "editor.inlineSuggest.enabled" = true;
+        "extensions.ignoreRecommendations" = true;
+        "explorer.confirmDragAndDrop" = false;
+
+        # Font
         "editor.fontLigatures" = true;
         "editor.fontFamily" = "CaskaydiaCove NF";
         "terminal.integrated.fontFamily" = "CaskaydiaCove NF";
-        "files.exclude" = {"**/node_modules/**" = true;};
-        "editor.formatOnSave" = true;
+
+        # Theme
         "workbench.colorTheme" = "Gruvbox Dark Hard";
         "workbench.iconTheme" = "material-icon-theme";
-        "window.titleBarStyle" = "custom";
+
+        # Git
+        "git.openRepositoryInParentFolders" = "always";
+        "git.autofetch" = true;
+
+        # Nix
+        "[nix]"."editor.tabSize" = 2;
+        "nix.enableLanguageServer" = true;
+
+        # C and CPP
+        "[c]"."editor.tabSize" = 4;
+        "[cpp]"."editor.tabSize" = 4;
         "C_Cpp.default.intelliSenseMode" = "linux-gcc-x64";
         "C_Cpp.default.cStandard" = "c99";
         "C_Cpp.codeAnalysis.clangTidy.enabled" = true;
-        "editor.inlineSuggest.enabled" = true;
-        "git.autofetch" = true;
+
+        # Python
+        "[python]"."editor.tabSize" = 4;
+
+        # HTML & CSS
+        "[html]"."editor.tabSize" = 2;
+        "[css]"."editor.tabSize" = 2;
+
+        # JavaScript & TypeScript
+        "[javascript]"."editor.tabSize" = 2;
+        "[javascriptreact]"."editor.tabSize" = 2;
+        "[typescript]"."editor.tabSize" = 2;
+        "[typescriptreact]"."editor.tabSize" = 2;
+
+        # Markdown
+        "[markdown]"."editor.tabSize" = 2;
       };
       extensions = with pkgs.vscode-extensions; [
         # C and CPP
         ms-vscode.cpptools
+
+        # Python
+        ms-python.python
+        ms-python.vscode-pylance
+        ms-toolsai.jupyter
 
         # Nix
         jnoortheen.nix-ide
@@ -44,9 +80,13 @@
 
         # Extra
         github.copilot
+
+        # HTML
       ];
 
-      package = pkgs.vscode;
+      package = pkgs.vscodium.override {
+        commandLineArgs = "--password-store='gnome' --enable-wayland-ime";
+      };
     };
   };
 
@@ -59,10 +99,15 @@
     gnumake
     valgrind
     gdb
+
+    # ms-python.python
+    python3
+
+    # jnoortheen.nix-ide
+    rnix-lsp
   ];
 
   # needed for store VS Code auth token
-  programs.seahorse.enable = true;
+  programs.seahorse.enable = !config.plasma.enable;
   services.gnome.gnome-keyring.enable = true;
-  nixpkgs.config.vscode.commandLineArgs = "--password-store='gnome'";
 }
