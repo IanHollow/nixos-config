d79299efb9a32bbe595a25355e4b70fd6f48104d
Ian Holloway <ian.m.holloway@gmail.com>
2023-12-03 00:03:50 -0800

    major overhaul to theming

--- modules/theming/cursor.nix
+++ modules/theming/cursor.nix
@@ -1,15 +1,16 @@
 {
   pkgs,
   vars,
+  config,
   ...
 }: {
   home-manager.users.${vars.user} = {
     home.pointerCursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
-      size = 24;
+      size = 22;
       gtk.enable = true;
-      x11.enable = true;
+      x11.enable = config.x11wm.enable;
     };
   };
 }
--- modules/theming/gtk.nix
+++ modules/theming/gtk.nix
@@ -1,18 +1,36 @@
 {
   vars,
   pkgs,
-  config,
   ...
 }: {
   home-manager.users.${vars.user} = {
-    # gtk's theme settings, generate files:
-    #   1. ~/.config/gtk-3.0/settings.ini
-    #   2. ~/.config/gtk-4.0/settings.ini
+    lib,
+    config,
+    ...
+  }: {
+    home.activation.removeExistingGTKConfig = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
+      rm -f "${config.xdg.configHome}/gtk-2.0/gtkrc"
+      rm -f "${config.xdg.configHome}/gtk-3.0/settings.ini"
+      rm -f "${config.xdg.configHome}/gtk-4.0/settings.ini"
+    '';
+
+    dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
     gtk = {
       enable = true;
 
+      gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+
+      gtk3.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+
+      gtk4.extraConfig = {
+        gtk-application-prefer-dark-theme = 1;
+      };
+
       font = {
-        name = "Noto Sans";
+        name = "Noto Sans 11";
         package = pkgs.noto-fonts;
       };
 
@@ -22,16 +40,24 @@
       };
 
       theme = {
-        # TODO: change to new theme and test that it works
-        # https://github.com/catppuccin/gtk
-        name = "Catppuccin-Macchiato-Compact-Pink-dark";
-        package = pkgs.catppuccin-gtk.override {
-          # https://github.com/NixOS/nixpkgs/blob/nixos-23.05/pkgs/data/themes/catppuccin-gtk/default.nix
-          accents = ["pink"];
-          size = "compact";
-          variant = "mocha";
-        };
+        package = pkgs.gruvbox-gtk-theme;
+
+        # Set the name of the theme
+        # make sure the name is correct otherwise gtk theme will not be applied
+        # you can check theme name by running: ls /nix/store/*-${package}-*/share/themes
+        # and look for theme based on your configuration
+        # if the theme does not apply then you selected the wrong name
+        name = "Gruvbox-Dark-BL";
       };
     };
   };
+
+  # enable dconf so that gtk and gnome apps work on none gnome desktops
+  programs.dconf.enable = true;
+
+  # For viewing gnome and gtk settings
+  # environment.systemPackages = with pkgs; [
+  #   gnome.gnome-tweaks
+  #   gnome.dconf-editor
+  # ];
 }
