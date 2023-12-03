2773ce76d04e5e7f46082cb5790a3340d4b54a55
Ian Holloway <ian.m.holloway@gmail.com>
2023-12-03 00:04:24 -0800

    Added TODO task to base input-components

--- modules/input-components/base.nix
+++ modules/input-components/base.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    # TODO: only add Japanese if the user wants it
     supportedLocales = ["en_US.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8"];
   };
 
