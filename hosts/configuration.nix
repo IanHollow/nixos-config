e3951cbe87051361d5b12dcd4d9448db12856c31
Ian Holloway <ian.m.holloway@gmail.com>
2023-12-13 18:15:27 -0800

    remove uneeded cache

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -53,13 +53,11 @@
       use-xdg-base-directories = true;
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       trusted-substituters = [
         "https://cache.nixos.org"
-        "https://nixpkgs-wayland.cachix.org"
         "https://nix-community.cachix.org"
         "https://cache.garnix.io"
       ];
