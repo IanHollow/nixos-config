146e12d6dd10453a895e7521430f218580da2cfc
Ian Holloway <ian.m.holloway@gmail.com>
2023-12-26 01:09:52 -0800

    change polkit to gnome and remove custom service
    This service was remove because it is only meant for desktop environments that do not start to the polkit automatically. However, it may be better to launch polkit in the desktop env

--- modules/security/polkit.nix
+++ modules/security/polkit.nix
@@ -2,27 +2,26 @@
   # Enable polkit
   security.polkit.enable = true;
 
-  # TODO: change this to gnome polkit
-
-  # Enable polkit-kde
+  # Enable polkit_gnome
   environment.systemPackages = with pkgs; [
-    polkit-kde-agent
+    polkit_gnome
   ];
 
   # Start Polkit Service
-  systemd = {
-    user.services.polkit-kde-agent-authentication-agent-1 = {
-      description = "polkit-kde-agent-authentication-agent-1";
-      wantedBy = ["graphical-session.target"];
-      wants = ["graphical-session.target"];
-      after = ["graphical-session.target"];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit-kde-agent}/libexec/polkit-kde-agent-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
-  };
+  # TODO: make this service an option to enable
+  # systemd = {
+  #   user.services.polkit-gnome-authentication-agent-1 = {
+  #     description = "polkit-gnome-authentication-agent-1";
+  #     wantedBy = ["graphical-session.target"];
+  #     wants = ["graphical-session.target"];
+  #     after = ["graphical-session.target"];
+  #     serviceConfig = {
+  #       Type = "simple";
+  #       ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+  #       Restart = "on-failure";
+  #       RestartSec = 1;
+  #       TimeoutStopSec = 10;
+  #     };
+  #   };
+  # };
 }
