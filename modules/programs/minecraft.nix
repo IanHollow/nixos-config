f5be711039f83a03caf51479ec0675c14f3e4e5a
Ian Holloway <ian.m.holloway@gmail.com>
2023-12-27 22:44:23 -0800

    added TODO to minecraft

--- modules/programs/minecraft.nix
+++ modules/programs/minecraft.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   # TODO: add options to install specific minecraft launchers or not
+  # TODO: add firewall rules to allow for LAN play
   environment.systemPackages = [
     inputs.getchoo.packages.${pkgs.system}.modrinth-app
     pkgs.prismlauncher
