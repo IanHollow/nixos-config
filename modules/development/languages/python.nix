e28d8c348fb3630fefb96efdf17d9fd7a97a9a9e
Ian Holloway <ian.m.holloway@gmail.com>
2024-01-07 18:37:01 -0800

    configure python more

--- modules/development/languages/python.nix
+++ modules/development/languages/python.nix
@@ -1,7 +1,38 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    python3
-    black # python formatter
-    yapf # python formatter
-  ];
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; {
+  options.python = {
+    enable = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable python support.";
+    };
+    finalPackage = mkOption {
+      type = with types; nullOr package;
+      readOnly = true;
+      description = "Resulting python package.";
+    };
+  };
+
+  config = let
+    my-python-packages = ps:
+      with ps; [
+        black # python formatter
+        yapf # python formatter
+      ];
+    finalPackage = pkgs.python3.withPackages my-python-packages;
+  in
+    mkIf (config.python.enable) {
+      # Install the final python package
+      environment.systemPackages = [
+        finalPackage
+      ];
+
+      # Set the final python package
+      python.finalPackage = finalPackage;
+    };
 }
