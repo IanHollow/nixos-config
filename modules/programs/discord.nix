08f9b825043534ad7699272495c50e9237b4bc64
Ian Holloway <ian.m.holloway@gmail.com>
2024-01-07 18:40:40 -0800

    discord is now webcord

--- modules/programs/discord.nix
+++ modules/programs/discord.nix
@@ -5,11 +5,6 @@
 }: {
   # TODO: add option to enable/disable discord
   environment.systemPackages = with unstable; [
-    # override the discord package to add extensions
-    (discord.override {
-      # remove any overrides that you don't want
-      withOpenASAR = true;
-      withVencord = true;
-    })
+    webcord-vencord
   ];
 }
