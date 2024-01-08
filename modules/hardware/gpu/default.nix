d1a5cc46e03d65d87d9387120cd4610960df1ddf
Ian Holloway <ian.m.holloway@gmail.com>
2024-01-07 18:46:19 -0800

    fixed erros in nvidia config and added more gpu config

--- modules/hardware/gpu/default.nix
+++ modules/hardware/gpu/default.nix
@@ -11,11 +11,29 @@
   # TODO: create global variable that defines a multi gpu system is enabled
 
   # TODO: check that at least one gpu is enabled to install packages below
+
+  # Install Extra Packages needed regardless of GPU
   environment.systemPackages = [
+    # Tools for debugging
     pkgs.libva
     pkgs.libva-utils # Good for debugging VA-API with "vainfo"
     pkgs.glxinfo
     pkgs.lshw # Good for indentifying hardware
-    unstable.ffmpeg-full
+
+    # Hardware acceleration
+    unstable.ffmpeg-full # TODO: explain why this is needed
+
+    # Vulkan
+    pkgs.vulkan-loader
+    pkgs.vulkan-tools
+    pkgs.vulkan-validation-layers
+    pkgs.vulkan-headers
+    pkgs.vulkan-utility-libraries
+    pkgs.vulkan-extension-layer
   ];
+
+  # Set environment variables for all gpus
+  environment.sessionVariables = {
+    WLR_RENDERER = "vulkan"; # Assuming that all gpus are vulkan capable
+  };
 }
--- modules/hardware/gpu/nvidia.nix
+++ modules/hardware/gpu/nvidia.nix
@@ -16,6 +16,15 @@ with lib; {
         default = false || nvidia_gpu.prime_render_offload.enable;
       };
 
+      # Enable direct backend for Nvidia
+      direct_backend = mkOption {
+        type = types.bool;
+        default = false;
+        description = ''
+          Enable direct backend for Nvidia.
+        ''; # TODO: add note to documentation about this option
+      };
+
       # Enable PRIME render offload
       prime_render_offload = {
         # Condition if host wants to use PRIME render offload
@@ -49,7 +58,8 @@ with lib; {
       services.xserver.videoDrivers = ["nvidia"];
 
       hardware.nvidia = let
-        nverProduction = config.boot.kernelPackages.nvidiaPackages.stable.version;
+        # TODO: add module to enable beta driver
+        nverProduction = config.boot.kernelPackages.nvidiaPackages.production.version;
         nverBeta = config.boot.kernelPackages.nvidiaPackages.beta.version;
         nvidiaPackage =
           if (lib.versionOlder nverBeta nverProduction)
@@ -73,7 +83,9 @@ with lib; {
         # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
         # Only available from driver 515.43.04+
         # Do not disable this unless your GPU is unsupported or if you have a good reason to.
-        open = true;
+        # NOTE: Due to issue https://github.com/NVIDIA/open-gpu-kernel-modules/issues/472
+        #       open kernel modules cannot be ennabled with hardware.nvidia.powerManagement.enabled set to true
+        open = !config.hardware.nvidia.powerManagement.enable;
 
         # The Nvidia settings menu,
         # accessible via `nvidia-settings`.
@@ -81,55 +93,57 @@ with lib; {
 
         # Uses beta version if it is newer than the production version
         package = nvidiaPackage;
+
+        # Nvidia Prime
+        prime = {
+          # Enable Nvidia Prime Offload
+          offload.enable = config.nvidia_gpu.prime_render_offload.enable;
+        };
       };
 
       # Nvidia DRM (Direct Rendering Manager) KMS (Kernel Mode Setting) support
       # NOTE: This is NOT the same as the other DRM (Digital Rights Management)
       # Based on Arch Wiki: https://wiki.archlinux.org/title/NVIDIA#DRM_kernel_mode_setting
       # Add kernel parameters to enable DRM KMS support
-      # NOTE: The kernel patameters for Nvidia use "-" instead of "_"
-      boot.kernelParams = [
-        # "nvidia-drm.modeset=1" # Based on Arch Wiki (Already enabled by NixOS but as "nvidia-drm.modeset=1")
-        "nvidia-drm.fbdev=1" # Based on Arch Wiki
-      ];
+      # NOTE: The kernel patameters for Nvidia use "-" instead of "_" which the Nvidia kernel modules use
+      boot.kernelParams =
+        [
+          # "nvidia-drm.modeset=1" # Already enabled by NixOS but as "nvidia-drm.modeset=1"
+          "nvidia-drm.fbdev=1" # Based on Arch Wiki
+        ]
+        # Enable Fix for Nvidia Suspend/WakeUp
+        ++ optionals (config.hardware.nvidia.powerManagement.enable) ["nvidia.NVreg_PreserveVideoMemoryAllocations=1"];
 
       # Early loading KMS support for Nvidia
       # Based on NixOS Wiki: https://nixos.wiki/wiki/Nvidia#Booting_to_Text_Mode
-      # Other Nvidia Kernel modules will be handled by NixOS as we don't want to load them out of order
-      boot.initrd.kernelModules = ["nvidia"];
+      # Also Based on Arch Wiki: https://wiki.archlinux.org/title/NVIDIA#Early_loading
+      # NOTE: VA-API will not work if Nvidia module "nvidia_uvm" is not loaded.
+      boot.initrd.kernelModules = ["nvidia" "nvidia_modeset" "nvidia_drm"];
+      boot.kernelModules = ["nvidia_uvm"];
       boot.extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
-    })
-
-    # Enable PRIME render offload
-    (mkIf (config.nvidia_gpu.prime_render_offload.enable) {
-      hardware.nvidia.prime = {
-        offload = {
-          enable = true; # Cannot be enabled alongside Nvidia Prime Sync
-          # enableOffloadCmd = true; # Provides `nvidia-offload` command. (can rewrite to make own version of command with more env vars)
-        };
-
-        # sync.enable = true; # Cannot be enabled alongside Nvidia Prime Offload
-
-        # reverseSync.enable = true;
-        # allowExternalGpu = false; # Enable if using an external GPU
-
-        # hardware-configuration.nix should specify the bus ID for integrated & nvidia gpus
-      };
 
       # Install Extra Packages for Nvidia
-      environment.systemPackages = [
+      environment.systemPackages =
+        []
         # Rewrote `nvidia-offload` command to include more environment variables
         # Based NixOS Wiki: https://nixos.wiki/wiki/Nvidia
         # Aditional env var of LIBVA_DRIVER_NAME=nvidia due to nvidia-vaapi-driver
-        (pkgs.writeShellScriptBin "nvidia-offload" ''
-          export __NV_PRIME_RENDER_OFFLOAD=1
-          export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-          export __GLX_VENDOR_LIBRARY_NAME=nvidia
-          export __VK_LAYER_NV_optimus=NVIDIA_only
-          export LIBVA_DRIVER_NAME=nvidia
-          exec "$@"
-        '')
-      ];
+        ++ optionals (config.nvidia_gpu.prime_render_offload.enable)
+        [
+          (pkgs.writeShellScriptBin "nvidia-offload" ''
+            export __NV_PRIME_RENDER_OFFLOAD=1
+            export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+            export __GLX_VENDOR_LIBRARY_NAME=nvidia
+            export __VK_LAYER_NV_optimus=NVIDIA_only
+            export LIBVA_DRIVER_NAME=nvidia
+            exec "$@"
+          '')
+        ];
+
+      # Enable Extra environment variables for Nvidia
+      environment.sessionVariables = {
+        NVD_BACKEND = mkIf (config.nvidia_gpu.direct_backend) "direct";
+      };
     })
   ];
 }
