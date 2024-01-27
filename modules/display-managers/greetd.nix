d97151900200f6aa67e13b1a568af9ae9fba94e2
Ian Holloway <ian.m.holloway@gmail.com>
2024-01-26 23:05:46 -0800

    try to implement regreet

--- modules/display-managers/greetd.nix
+++ modules/display-managers/greetd.nix
@@ -6,7 +6,9 @@
   lib,
   ...
 }:
-with lib; {
+with lib; let
+  home_config = config.home-manager.users.${vars.user};
+in {
   # Define options for the greetd module
   options.greetd = {
     # Enable the greetd module
@@ -105,7 +107,6 @@ with lib; {
 
         # Configure the greetd settings
         settings = let
-          # TODO: setup ReGreet or gtkgreet as greeters
           # Define greetd-tui
           greetd_tui_exec = "${pkgs.greetd.tuigreet}/bin/tuigreet";
           greetd_tui_args = strings.concatStringsSep " " [
@@ -114,6 +115,15 @@ with lib; {
             "--sessions /etc/greetd/wayland-sessions:/etc/greetd/xsessions"
           ];
           greetd_tui_command = "${greetd_tui_exec} ${greetd_tui_args}";
+
+          # Define ReGreet
+          regreet_exec = "${pkgs.greetd.regreet}/bin/regreet";
+          cage_exec = "${pkgs.cage}/bin/cage";
+          cage_args = strings.concatStringsSep " " [
+            "-s"
+            "--"
+          ];
+          regreet_command = "${cage_exec} ${cage_args} ${regreet_exec}";
         in {
           # Set the default session
           default_session = {
@@ -129,6 +139,7 @@ with lib; {
         };
       };
 
+      # Store sessions in in /etc for the greetd service
       environment.etc = let
         # Determine path for the sessions
         sessions =
@@ -148,6 +159,39 @@ with lib; {
           group = "root"; # Set group to root
         };
       };
+
+      # Configure ReGreet greeter
+      # programs.regreet = {
+      #   enable = true;
+      #   package = pkgs.greetd.regreet;
+      #   settings = {
+      #     # background = {
+      #     #   path = "";
+      #     #   # Available values: "Fill", "Contain", "Cover", "ScaleDown"
+      #     #   fit = "";
+      #     # };
+      #     # env = {};
+      #     gtk = {
+      #       application_prefer_dark_theme = true;
+      #       cursor_theme_name = home_config.home.pointerCursor.name;
+      #       font_name = home_config.gtk.font.name;
+      #       icon_theme_name = home_config.gtk.iconTheme.name;
+      #       theme_name = home_config.gtk.theme.name;
+      #     };
+      #     commands = {
+      #       # The command used to reboot the system
+      #       reboot = ["systemctl" "reboot"];
+
+      #       # The command used to shut down the system
+      #       poweroff = ["systemctl" "poweroff"];
+      #     };
+      #   };
+      # };
+
+      # Set environment variables for the ReGreet greeter
+      # environment.variables = {
+      #   SESSION_DIRS = "/etc/greetd/wayland-sessions:/etc/greetd/xsessions";
+      # };
 
       # Configure keyring
       security.pam.services.greetd.enableGnomeKeyring = mkIf (config.keyring.enable) true;
