185a8cc108c87e3607c26843c0e83bf629bf9f15
Ian Holloway <ian.m.holloway@gmail.com>
2024-01-26 23:08:27 -0800

    add support for clang

--- modules/development/languages/c.nix
+++ modules/development/languages/c.nix
@@ -1,8 +1,21 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    gcc
+    # builder
     gnumake
-    valgrind
+
+    # debugger
+    llvmPackages.lldb
     gdb
+
+    # fix headers not found
+    clang-tools
+
+    # LSP and compiler
+    llvmPackages.libstdcxxClang
+    gcc
+
+    # other tools
+    llvmPackages.libllvm
+    valgrind
   ];
 }
